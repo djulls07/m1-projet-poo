e2f5ba3f9487c58108c8f8aada124f523a15041e
Jules L <djulls07@gmail.com>
2014-01-09 07:54:13 +0100

    ajout de la main ( useless et presque vide !)

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "include/PlateauP4.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    PlateauP4* p = new PlateauP4();
+    cout << p->getTailleV() << endl;
+    p->afficher();
+    delete(p);
+    return 0;
+}
